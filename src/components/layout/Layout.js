9973e5aa11d86e7e13da55dabf8bb8285a06a11c
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-01 12:47:53 +0430

    debug layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Layout = ({ children }) => {
-  return <main className='w-96  mx-auto min-h-screen my-5 '>{children}</main>;
+  return <main className='w-3/4  mx-auto h-screen my-5 '>{children}</main>;
 };
 
 export default Layout;
