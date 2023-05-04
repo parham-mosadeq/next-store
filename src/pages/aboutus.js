86bf0d783ffd6b3b1d7abb5da052ef4035a2ef92
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-04 21:05:43 +0430

    create about us boilerplate

---
+++ src/pages/aboutus.js
@@ -0,0 +1,18 @@
+import Layout from '@/components/layout/Layout';
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <Layout>
+      <div className='mb-5'>
+        <h3>Welcome To shop-P</h3>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex perferendis
+        omnis totam quis fugit iusto. Eveniet aperiam quis sed molestias.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutUsPage;
